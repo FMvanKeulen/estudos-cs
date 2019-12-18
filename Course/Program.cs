f04c6493a1d49e20a9a552ed3db441afcd471503
Fillipe van Keulen <fmvankeulen@gmail.com>
2019-12-17 22:09:50 -0300

    Primeiro commit do projeto Course

---
+++ Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Delivered
+            };
+
+            Console.WriteLine(order);
+        }
+    }
+}
