af062dc3b5ddd3df5a6f3e7d6c6b147ed82232cf
Fillipe van Keulen <fmvankeulen@gmail.com>
2019-12-18 08:06:20 -0300

    Outros projetos adicionados

---
+++ Timestamp/Timestamp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Timestamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            TimeSpan t2 = new TimeSpan();
+            TimeSpan t3 = new TimeSpan(900000000L);
+            TimeSpan t4 = new TimeSpan(2, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t6 = new TimeSpan(1, 2, 11, 21, 321);
+            TimeSpan t7 = TimeSpan.FromDays(1.5);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+            Console.WriteLine(t7);
+        }
+    }
+}
